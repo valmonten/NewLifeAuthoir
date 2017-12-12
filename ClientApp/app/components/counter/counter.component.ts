5fbc1a683ec2c1604693333dcfa0259264b77386
Nathan Valmonte <valmonten@gmail.com>
2017-12-12 11:48:13 -0800

    Added slidetoggle to agenda journal and notes

--- ClientApp/app/components/counter/counter.component.ts
+++ ClientApp/app/components/counter/counter.component.ts
@@ -1,26 +1,136 @@
 import { Component, Inject } from '@angular/core';
 import { Http } from '@angular/http';
+import * as $ from "jquery";
 
 @Component({
     selector: 'counter',
     templateUrl: './counter.component.html'
 })
 export class CounterComponent {
-    public currentCount = 0;
-    public person: Users;
+    public something = "Something";
+    toggleNotes_m(){
+        $(".notesContent_m").slideDown();
+        $(".journalContent_m").slideUp();
+        $(".agendaContent_m").slideUp();
+        console.log("Notes")
+    }
+    toggleJournal_m(){
+        $(".journalContent_m").slideDown();
+        $(".notesContent_m").slideUp();
+        $(".agendaContent_m").slideUp();
+    }
+    toggleAgenda_m(){
+        $(".journalContent_m").slideUp();
+        $(".notesContent_m").slideUp();
+        $(".agendaContent_m").slideDown();
+    }
+    toggleNotes_tu(){
+        $(".notesContent_tu").slideDown();
+        $(".journalContent_tu").slideUp();
+        $(".agendaContent_tu").slideUp();
+        console.log("Notes")
+    }
+    toggleJournal_tu(){
+        $(".journalContent_tu").slideDown();
+        $(".notesContent_tu").slideUp();
+        $(".agendaContent_tu").slideUp();
+    }
+    toggleAgenda_tu(){
+        $(".journalContent_tu").slideUp();
+        $(".notesContent_tu").slideUp();
+        $(".agendaContent_tu").slideDown();
+    }
+    toggleNotes_w(){
+        $(".notesContent_w").slideDown();
+        $(".journalContent_w").slideUp();
+        $(".agendaContent_w").slideUp();
+    }
+    toggleJournal_w(){
+        $(".journalContent_w").slideDown();
+        $(".notesContent_w").slideUp();
+        $(".agendaContent_w").slideUp();
+    }
+    toggleAgenda_w(){
+        $(".journalContent_w").slideUp();
+        $(".notesContent_w").slideUp();
+        $(".agendaContent_w").slideDown();
+    }
+    toggleNotes_th(){
+        $(".notesContent_th").slideDown();
+        $(".journalContent_th").slideUp();
+        $(".agendaContent_th").slideUp();
+    }
+    toggleJournal_th(){
+        $(".journalContent_th").slideDown();
+        $(".notesContent_th").slideUp();
+        $(".agendaContent_th").slideUp();
+    }
+    toggleAgenda_th(){
+        $(".journalContent_th").slideUp();
+        $(".notesContent_th").slideUp();
+        $(".agendaContent_th").slideDown();
+    }
+    toggleNotes_f(){
+        $(".notesContent_f").slideDown();
+        $(".journalContent_f").slideUp();
+        $(".agendaContent_f").slideUp();
+    }
+    toggleJournal_f(){
+        $(".journalContent_f").slideDown();
+        $(".notesContent_f").slideUp();
+        $(".agendaContent_f").slideUp();
+    }
+    toggleAgenda_f(){
+        $(".journalContent_f").slideUp();
+        $(".notesContent_f").slideUp();
+        $(".agendaContent_f").slideDown();
+    }
+    toggleNotes_sa(){
+        $(".notesContent_sa").slideDown();
+        $(".journalContent_sa").slideUp();
+        $(".agendaContent_sa").slideUp();
+    }
+    toggleJournal_sa(){
+        $(".journalContent_sa").slideDown();
+        $(".notesContent_sa").slideUp();
+        $(".agendaContent_sa").slideUp();
+    }
+    toggleAgenda_sa(){
+        $(".journalContent_sa").slideUp();
+        $(".notesContent_sa").slideUp();
+        $(".agendaContent_sa").slideDown();
+    }
+    toggleNotes_su(){
+        $(".notesContent_su").slideDown();
+        $(".journalContent_su").slideUp();
+        $(".agendaContent_su").slideUp();
+    }
+    toggleJournal_su(){
+        $(".journalContent_su").slideDown();
+        $(".notesContent_su").slideUp();
+        $(".agendaContent_su").slideUp();
+    }
+    toggleAgenda_su(){
+        $(".journalContent_su").slideUp();
+        $(".notesContent_su").slideUp();
+        $(".agendaContent_su").slideDown();
+    }
+
+
+
 
     
-    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
-        http.get(baseUrl + 'api/SampleData/Usersdata').subscribe(result => {
-            this.person = result.json() as Users;
-        }, error => console.error(error));
-    }
+    // constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+    //     http.get(baseUrl + 'api/SampleData/Usersdata').subscribe(result => {
+    //         this.person = result.json() as Users;
+    //     }, error => console.error(error));
+    // }
 }
-interface Users {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
+// interface Users {
+//     id: number;
+//     name: string;
+//     email: string;
+//     password: string;
     
-}
+// }
 
