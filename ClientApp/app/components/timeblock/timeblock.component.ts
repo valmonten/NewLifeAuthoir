e67ecd84f5ae9ea85b9215a0aaf92584cc32bb4d
Nathan Valmonte <valmonten@gmail.com>
2017-12-14 15:38:59 -0800

    Added new component timeblock

---
+++ ClientApp/app/components/timeblock/timeblock.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'timeblock',
+    templateUrl: './timeblock.component.html'
+}) 
+export class TimeblockComponent {
+    public imhere: string ="Hello";
+}
