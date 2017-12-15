7da91040f6ec1bebef88331f7f16db934814cd2f
Jeff Dugan <jeffdugan5@gmail.com>
2017-12-15 07:53:00 -0800

    added array

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -12,6 +12,9 @@ export class AppComponent {
     public today: Date;
     public weekday: Date;
     public resulting: any;
+    public currentweek: Array<number>;
+    public dayarray: Array<Date>;
+    //public daybefore :Date;
 
     constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
         http.get(baseUrl + 'api/SampleData/CalendarData').subscribe(result => {
@@ -19,46 +22,59 @@ export class AppComponent {
             this.today = result.json().dt;
             this.weekday = result.json().dw;
             this.resulting = result.json();
+            // this.daybefore = result.json().dt;
 
            
         }, error => console.error(error));
 
+        http.get(baseUrl + 'api/SampleData/TheWeek').subscribe(result => {
+            this.currentweek = result.json(); 
+
+        }, error => console.error(error));
+
+        http.get(baseUrl + 'api/SampleData/DayOfWeek').subscribe(result => {
+            this.dayarray = result.json(); 
+
+        }, error => console.error(error));
+
+    
+
     }
 
-    public Monday: string = "Monday"
-    public monthday1 = 5;
+    public Sunday: string = "Sunday"
+    public monthday1: Date = this.today;
 
-    public Tuesday: string = "Tuesday"
+    public Monday: string = "Monday"
     public monthday2: Date = this.today;
 
-    public Wednesday: string = "Wednesday"
+    public Tuesday: string = "Tuesday"
     public monthday3: Date = this.today;
 
-    public Thursday: string = "Thursday"
+    public Wednesday: string = "Wednesday"
     public monthday4: Date = this.today;
 
-    public Friday: string = "Friday"
+    public Thursday: string = "Thursday"
     public monthday5: Date = this.today;
 
-    public Saturday: string = "Saturday"
+    public Friday: string = "Friday"
     public monthday6: Date = this.today;
 
-    public Sunday: string = "Sunday"
+    public Saturday: string = "Saturday"
     public monthday7: Date = this.today;
 
 }
 
 interface Calendar{
-    calendarid: Date;
+    calendarid: number;
     dt: Date;
-    y: Date;
-    q: Date;
-    m: Date;
-    d: Date;
-    dw: Date;
+    y: number;
+    q: number;
+    m: number;
+    d: number;
+    dw: number;
     monthName: string;
     dayName: string;
-    w: Date;
+    w: number;
 
 
 
