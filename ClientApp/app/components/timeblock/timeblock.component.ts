438f302b24c32c9e745fd8fb1fca90b7654c755f
Nathan Valmonte <valmonten@gmail.com>
2017-12-18 16:35:07 -0800

    Showing hardcoded event data on hover

--- ClientApp/app/components/timeblock/timeblock.component.ts
+++ ClientApp/app/components/timeblock/timeblock.component.ts
@@ -9,12 +9,18 @@ export class TimeblockComponent {
     public imhere: string ="Hello";
     @Input() hour: string;
     @Input() weekday: string;
-
+    public variance: string = "things show here. If there is more stuff it goes here and then here";
     newevent()
     {
         
         $("."+this.weekday+"addevent").slideDown();
         $("."+this.weekday+"timeblocks").slideUp();
+    }
+    mousein(){
+        $("."+this.weekday+"readevent").html(this.variance);
+    }
+    mouseout(){
+        $("."+this.weekday+"readevent").html("");
     }
     
     //'<form><label>Time: <input type="text" name="time"></label><label>Title: <input type="text" name="title"></label><label>Description:<textarea></textarea></label><select name="genre"><option value="Work">Work</option><option value="Social">Social</option><option value="Health">Health</option><option value="Financial">Financial</option>      <option value="Spiritual">Spiritual</option><option value="Intellectual">Intellectual</option></select><input type="submit" value="Submit"></form>')
