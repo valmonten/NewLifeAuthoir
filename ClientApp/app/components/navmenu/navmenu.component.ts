693162245152bc5ebaff6d82e0f6c9319401ff55
Nathan Valmonte <valmonten@gmail.com>
2017-12-20 16:50:02 -0800

    Notes and Journal now show consistently

--- ClientApp/app/components/navmenu/navmenu.component.ts
+++ ClientApp/app/components/navmenu/navmenu.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, Injectable, Input, Output, EventEmitter } from '@ang
 import { Http, Response, RequestOptions, Headers, Request, RequestMethod } from '@angular/http';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import * as $ from "jquery";
+import { dispatchEvent } from '@angular/core/src/view/util';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class NavMenuComponent {
     @Output() toshow = new EventEmitter();
     @Input() weekday: string;
     @Input() date: Date;
-    public detail: object;
+    public detail: string;
     public DisplayNote: Array<string>;
     public dayarray: Array<Date>;
     public Entry: string;
@@ -23,20 +24,44 @@ export class NavMenuComponent {
     constructor(private http: Http, @Inject('BASE_URL') private baseUrl: string) {
         http.get(baseUrl + 'api/SampleData/DayOfWeek').subscribe(result => {
             this.dayarray = result.json();
+            console.log("Show me the day array "+this.dayarray);
             
         }, error => console.error(error));
     
         http.get(baseUrl + 'api/SampleData/NoteData').subscribe(result => {
             this.DisplayNote = result.json();
-    
-            for( let entry in this.dayarray){
-                if (this.date == this.dayarray[entry]){
-                //fix null exceptions with if statemnt 
-                    // console.log(this.DisplayNote[entry])
-                    this.Entry = this.DisplayNote[entry];
-                    // console.log(this.Entry);
-                }
+            
+            if(this.weekday == "Sunday"){
+                this.detail= this.DisplayNote[0]
             }
+            if(this.weekday == "Monday"){
+                this.detail= this.DisplayNote[1]
+            }
+            if(this.weekday == "Tuesday"){
+                this.detail= this.DisplayNote[2]
+            }
+            if(this.weekday == "Wednesday"){
+                this.detail= this.DisplayNote[3]
+            }
+            if(this.weekday == "Thursday"){
+                this.detail= this.DisplayNote[4]
+            }
+            if(this.weekday == "Friday"){
+                this.detail= this.DisplayNote[5]
+            }
+            if(this.weekday == "Saturday"){
+                this.detail= this.DisplayNote[6]
+            }
+            
+            
+            // for( let i in this.dayarray){
+            //     if (this.date == this.dayarray[i]){
+            //     //fix null exceptions with if statemnt 
+            //         // console.log(this.DisplayNote[i])
+            //         this.Entry = this.DisplayNote[i];
+            //         console.log(this.Entry+ " Notes are in this beezy");
+            //     }
+            // }
             
         }, error => console.error(error));
     }
@@ -47,8 +72,8 @@ export class NavMenuComponent {
         $("."+this.weekday+"_j_content").slideUp();
     }
     save() {
-        var data: Array<object> =[this.detail, this.date]
-        // console.log(JSON.stringify(data[1])); 
+        var data: Array<any> =[this.detail, this.date]
+        console.log(JSON.stringify(data[1])); 
         this.postData(data)  
             .subscribe(  
             (response) => {  
